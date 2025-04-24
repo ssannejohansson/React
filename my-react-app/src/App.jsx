651baad49a4322bd2048607bda9190c03dc645e8
ssannejohansson <sannej0hansson@hotmail.com>
2025-04-24 09:35:35 +0200

    Created nav
    Created nav, style later

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,18 +1,22 @@
 import Project from './components/Projects.jsx'
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from "./pages/home.jsx";
 import About from "./pages/about.jsx";
+import Navbar from './navbar.jsx';
 
 function App() {
 
-  return <>
-  <BrowserRouter>
+  return (
+  <>
+  <BrowserRouter>  
+  <Navbar></Navbar>
   <Routes>
     <Route path="/" element={<Home/>}/>
     <Route path="/about" element={<About/>}/>
   </Routes>
   </BrowserRouter>
-    </>;
+    </>
+  );
   
 }
 
